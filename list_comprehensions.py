3802d9b3cbd07956aa23f062cf6b9a1588626a31
Yusif Hasanli <yusifhasanlien@gmail.com>
2025-07-07 16:44:32 +0400

    Create list_comprehensions.py

---
+++ list_comprehensions.py
@@ -0,0 +1,28 @@
+tuple_xx = ("hi",2,3,)
+print(tuple_xx)
+x= 1,
+print(type(x))
+
+tuple_mary=("germany",11.2,)
+tuple_leila=("uk",12.2,)
+tuple_rinat=("usa",23.4,)
+
+# zz = type(tuple_rinat)
+
+y  = tuple_mary[0] #DUMB YUSIF TUPILS ARE IMMUTABLE U CAN'T CHANGE THE PLACES OF VALUE, BUT U CAN MAKE NEW TUPLE USING IT
+f = tuple_mary[1]
+
+EMPTY_TUPLE=tuple([f,y,])
+print(EMPTY_TUPLE[1])
+# for i in tuple_mary:
+#     EMPTY_TUPLE.append(-1)
+#     print(EMPTY_TUPLE)
+
+#dictionary
+x_dictionary = {
+    "name " : "yusif",
+"age " : 13,
+"city" : "new york"
+}
+
+print(x_dictionary)
