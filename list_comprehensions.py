54ada37165ec95c4f8b3bac12d456bf99c4f1a59
Yusif Hasanli <yusifhasanlien@gmail.com>
2025-07-08 17:38:31 +0400

    Update list_comprehensions.py

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -26,3 +26,30 @@ x_dictionary = {
 }
 
 print(x_dictionary)
+
+met_museum = {"artist" : "Ryūryūkyo Shinsai","period":"19th century","date":1930}
+print(met_museum)
+
+list1 = ["a","b","c",1] #ordered
+tuple1=  ("a","b","c",1,2,1,2,4) #ordered
+set1 = set(tuple1) #sets are un-ordered
+dict1 = {
+    "value1":"a","value2":"b","value3":"c","value4":1
+}
+print(tuple1)
+print(set1)
+
+set1 = {'alma','nar','banana'}
+set2=set(1,2,"nar","na")
+print(set2)
+
+
+#sets are for store uniqe values
+my_fav={"apple","banana","kiwi"}
+friends_fav={"orange","strawberry","kiwi","plum","banana"}
+
+z = my_fav.intersection (friends_fav) #can use '&'
+y = my_fav.union(friends_fav) # can use '|'
+
+print(z)
+print(y)
